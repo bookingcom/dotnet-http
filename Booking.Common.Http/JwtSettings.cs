79a85c4cfee21501b0e516b44ebaaca08f4ab414
Altinok Darici <altinok.darici@booking.com>
2018-06-26 15:38:36 +0200

    Hard coded the token endpoint.

--- Booking.Common.Http/JwtSettings.cs
+++ Booking.Common.Http/JwtSettings.cs
@@ -1,11 +1,15 @@
 ﻿namespace Booking.Common.Http
 {
-    public class JwtSettings
-    {
+	public class JwtSettings
+	{
+		public string ClientId { get; set; }
+		public string ClientSecret { get; set; }
+		public string TokenEndpoint { get; set; }
+		public string Resource { get; set; }
 
-        public string ClientId { get; set; }
-        public string ClientSecret { get; set; }
-        public string TokenEndpoint { get;set; }
-        public string Resource { get; set; }
-    }
+		public JwtSettings()
+		{
+			TokenEndpoint = "https://login.microsoftonline.com/a430c977-2786-4a63-a50e-90b71e55d889/oauth2/token";
+		}
+	}
 }
