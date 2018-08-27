3c4df4407a66753d3da85a0c4240f5538d12d422
Altinok Darici <altinok.darici@booking.com>
2018-08-27 16:42:56 +0200

    Fixing thread safe issue for HttpClient

--- Booking.Common.Rest/RestClient.cs
+++ Booking.Common.Rest/RestClient.cs
@@ -1,22 +1,22 @@
-﻿using System;
+﻿using Booking.Common.HttpClient.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Booking.Common.HttpClient.Extensions;
 
 namespace Booking.Common.Rest
 {
 	public abstract class RestClient<TResponse> : RestClient<TResponse, string, object>
 	{
+		[Obsolete("Use RestClient(HttpClient client) instead.", true)]
 		public RestClient(string baseAddress)
 			: base(baseAddress)
 		{
 		}
+		[Obsolete("Use RestClient(HttpClient client) instead.", true)]
 		public RestClient(System.Net.Http.HttpClient client, string baseAddress)
 			: base(client, baseAddress)
 		{
-
-
 		}
 
 		public RestClient(System.Net.Http.HttpClient client)
@@ -24,15 +24,16 @@ namespace Booking.Common.Rest
 		{
 		}
 
+		[Obsolete("Use RestClient(HttpClient client) instead.", true)]
 		public RestClient(HttpMessageHandler handler, string baseAddress)
 			: base(handler, baseAddress)
 		{
 		}
 	}
 
-
 	public abstract class RestClient<TResponse, TKey, TBody> : RestClient, IRestClient<TResponse, TKey, TBody>
 	{
+		[Obsolete("Use RestClient(HttpClient client) instead.", true)]
 		public RestClient(string baseAddress)
 			: base(baseAddress)
 		{
@@ -43,11 +44,13 @@ namespace Booking.Common.Rest
 		{
 		}
 
+		[Obsolete("Use RestClient(HttpClient client) instead.", true)]
 		public RestClient(System.Net.Http.HttpClient client, string baseAddress)
 			: base(client, baseAddress)
 		{
 		}
 
+		[Obsolete("Use RestClient(HttpClient client) instead.", true)]
 		public RestClient(HttpMessageHandler handler, string baseAddress)
 			: base(handler, baseAddress)
 		{
@@ -55,29 +58,30 @@ namespace Booking.Common.Rest
 
 		public virtual Task<IEnumerable<TResponse>> GetAsync()
 		{
-			return HttpClient.GetAsync<IEnumerable<TResponse>>();
+			return HttpClient.GetAsync<IEnumerable<TResponse>>($"{ControllerName}");
 		}
 		public virtual Task<TResponse> GetAsync(TKey id)
 		{
-			return HttpClient.GetAsync<TResponse>(id.ToString());
+			return HttpClient.GetAsync<TResponse>($"{ControllerName}{id}");
 		}
 
 		public virtual Task<TResponse> PostAsync(TBody model)
 		{
-			return HttpClient.PostAsync<TResponse>(model);
+			return HttpClient.PostAsync<TResponse>($"{ControllerName}", model);
 		}
 
 		public virtual Task DeleteAsync(TKey id)
 		{
-			return HttpClient.DeleteAsync(id.ToString());
+			return HttpClient.DeleteAsync($"{ControllerName}{id}");
 		}
 	}
-
 
 	public abstract class RestClient : IRestClient
 	{
 		protected System.Net.Http.HttpClient HttpClient { get; }
+		protected string ControllerName => this.GetType().Name.Replace("Client", "/");
 
+		[Obsolete("Use RestClient(HttpClient client) instead.", true)]
 		public RestClient(string baseAddress)
 			: this(new HttpClientHandler(), baseAddress)
 		{
@@ -88,12 +92,15 @@ namespace Booking.Common.Rest
 		{
 			HttpClient = client;
 		}
+
+		[Obsolete("Use RestClient(HttpClient client) instead.", true)]
 		public RestClient(System.Net.Http.HttpClient client, string baseAddress) : this(client)
 		{
 			string controller = this.GetType().Name.Replace("Client", "/");
 			HttpClient.BaseAddress = new Uri(new Uri(baseAddress), controller);
 		}
 
+		[Obsolete("Use RestClient(HttpClient client) instead.", true)]
 		public RestClient(HttpMessageHandler handler, string baseAddress)
 			: this(new System.Net.Http.HttpClient(handler), baseAddress)
 		{
